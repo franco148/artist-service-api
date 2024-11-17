1a1b107075ce02a395502de38135ccfa98616448
Franco Arratia <franck_ral@hotmail.com>
2024-11-17 18:33:24 -0400

    Initial commit

---
+++ src/test/java/com/francofral/artistapi/ArtistServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francofral.artistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
